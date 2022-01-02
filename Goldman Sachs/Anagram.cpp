90868ee4322a25dc9fede051c40b3ee229fe504d
abhishekg0907 <89632449+abhishekg0907@users.noreply.github.com>
2022-01-02 11:18:27 +0530

    Print Anagrams Together.cpp

--- Goldman Sachs/Anagram.cpp
+++ Goldman Sachs/Anagram.cpp
@@ -1,22 +1,30 @@
-class Anagram{
-	public:
-	vector<vector<string> > anagram(vector<string> in){
-			
-	unordered_map<string,vector<string> > mp;
-	
-	unordered_map<string,vector<string> > :: iterator j;
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        unordered_map<string,vector<string> > mp;
+    	unordered_map<string,vector<string> > :: iterator j;
 	
 	vector<vector<string> > out;
-	for(int i=0;i<in.size();i++)
+	for(int i=0;i<string_list.size();i++)
 	{
-		string word = in[i];
+		string word = string_list[i];
 		sort(word.begin(),word.end());
-		mp[word].push_back(in[i]);
+		mp[word].push_back(string_list[i]);
 	}
 	
 	for(j=mp.begin();j!=mp.end();j++)
 		out.push_back(j->second);
 	
 	return out;
-}
+    }
 };
